30de77f935cc3f6052e716853d4d4da7796bd6cb
Chanakan393 <chana.sch17@gmail.com>
2025-10-07 23:03:16 +0700

    Form testing

--- tests/tests/example.spec.ts
+++ tests/tests/example.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Basic Form submit', async ({ page }) => {
+  // 2.3
   await page.goto('http://localhost:8080/'); // เปิดเว็บ
   await page.fill('input[label="Name"]', 'Aod');  // ใส่ค่า Name
   await page.fill('input[label="Email"]', 'aod@example.com'); // ใส่ค่า Email
@@ -9,4 +10,12 @@ test('Basic Form submit', async ({ page }) => {
   // ตรวจสอบผลลัพธ์ที่แสดง
   await expect(page.locator('text=Aod')).toBeVisible();
   await expect(page.locator('text=aod@example.com')).toBeVisible();
+
+  // 2.4
+  // ตรวจสอบ input values หลังกรอก
+  await expect(page.locator('input[label="Name"]')).toHaveValue('Aod');
+  await expect(page.locator('input[label="Email"]')).toHaveValue('aod@example.com');
+
+  // ตรวจสอบปุ่ม Submit
+  await expect(page.locator('button[type="submit"]')).toBeEnabled();
 });
